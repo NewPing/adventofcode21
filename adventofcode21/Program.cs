8d0e600fcd80a903e2d6200503f3d24dd4a179b8
NewPing <jan-roth1998@web.de>
2021-12-04 04:37:04 +0100

    solution for day1

--- adventofcode21/Program.cs
+++ adventofcode21/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using adventofcode21.puzzles;
+
 Console.WriteLine("-< Advent of Code 2021 >-");
+//new Day1();
+new day2();
---
+++ adventofcode21/puzzles/day1/Day1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adventofcode21.puzzles
+{
+    internal class Day1
+    {
+        public Day1()
+        {
+            sweep1();
+            sweep2();
+        }
+
+        public void sweep1()
+        {
+            var sweeps = File.ReadAllLines("Sonar Sweep.txt");
+            var increases = 0;
+            var decreases = 0;
+            var noChanges = 0;
+            for (int i = 1; i < sweeps.Length; i++)
+            {
+                Console.Write(sweeps[i]);
+                if (int.Parse(sweeps[i]) > int.Parse(sweeps[i - 1]))
+                {
+                    Console.WriteLine(" increased");
+                    increases++;
+                }
+                else if (int.Parse(sweeps[i]) < int.Parse(sweeps[i - 1]))
+                {
+                    Console.WriteLine(" decreased");
+                    decreases++;
+                }
+                else
+                {
+                    Console.WriteLine(" no change");
+                    noChanges++;
+                }
+            }
+            Console.WriteLine("increases: " + increases);
+            Console.WriteLine("decreases: " + decreases);
+            Console.WriteLine("no changes: " + noChanges);
+        }
+
+        public void sweep2()
+        {
+            var sweeps = File.ReadAllLines("Sonar Sweep.txt");
+            var increases = 0;
+            var decreases = 0;
+            var noChanges = 0;
+            var sum = 0;
+            int oldSum;
+            for (int i = 2; i < sweeps.Length -1; i++)
+            {
+                oldSum = sum;
+                sum = int.Parse(sweeps[i - 1]) + int.Parse(sweeps[i]) + int.Parse(sweeps[i + 1]);
+
+                Console.Write(sweeps[i]);
+                if (sum > oldSum)
+                {
+                    Console.WriteLine(" increased");
+                    increases++;
+                } else if (sum < oldSum)
+                {
+                    Console.WriteLine(" decreased");
+                    decreases++;
+                } else
+                {
+                    Console.WriteLine(" no change");
+                    noChanges++;
+                }
+            }
+            Console.WriteLine("increases: " + increases);
+            Console.WriteLine("decreases: " + decreases);
+            Console.WriteLine("no changes: " + noChanges);
+        }
+    }
+}
